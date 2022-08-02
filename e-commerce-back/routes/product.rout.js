c31ace789457b1a49822677235006025527cffb8
Mohamed Abdelwahed <memoelfares613@gmail.com>
2022-08-02 21:25:15 +0200

    after adding product route and control

--- e-commerce-back/routes/product.rout.js
+++ e-commerce-back/routes/product.rout.js
@@ -1,7 +1,12 @@
 const router = require ("express").Router()
-const user = require("../app/controllers/product.controller")
+const product = require("../app/controllers/product.controller")
+const {auth, authAdmin} = require("../middleware/auth.middleware")
 
-// router.post("/register", user.register)
+router.post("/add",authAdmin,product.addProduct)
+router.get("/getSingle/:id",authAdmin,product.getSingle)
+router.get("/myProducts",auth,product.myProducts)
+router.delete("/delProduct",authAdmin,product.delProduct)
+router.post("/editProduct",authAdmin,product.editProduct)
 
 
 module.exports = router 
